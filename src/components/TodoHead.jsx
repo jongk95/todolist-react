e5303ecaf168e1288e02e13d59c5e8cf70ce3604
jongk95 <53596817+yuonx95@users.noreply.github.com>
2020-06-29 11:54:21 +0900

    react todo

---
+++ src/components/TodoHead.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+  padding-top: 48px;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 24px;
+  border-bottom: 1.5px solid #e9ecef;
+
+  h1 {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+
+  .tasks-left {
+    color: #20c997;
+    font-size: 18px;
+    margin-top: 40px;
+    font-weight: bold;
+  }
+`;
+
+function TodoHead() {
+  return (
+    <>
+      <TodoHeadBlock>
+        <h1>2020-06-29</h1>
+        <div className="day">Monday</div>
+        <div className="tasks-left">2 Things Left</div>
+      </TodoHeadBlock>
+    </>
+  );
+}
+
+export default TodoHead;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import TodoItem from "./TodoItem";
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+  return (
+    <>
+      <TodoListBlock>
+        <TodoItem text="project create" done={true} />
+        <TodoItem text="project create" done={true} />
+        <TodoItem text="project create" done={false} />
+        <TodoItem text="project create" done={true} />
+      </TodoListBlock>
+    </>
+  );
+}
+
+export default TodoList;
